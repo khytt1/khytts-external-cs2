8b617efe289e876da3a1c221051a2d33b9b055a9
khytt <drapersimpson29@gmail.com>
2026-03-10 12:05:30 -0500

    Add files via upload

--- output/worldrenderer_dll.cs
+++ output/worldrenderer_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-03-05 02:57:11.470358600 UTC
+// 2026-03-10 17:03:29.848101700 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: worldrenderer.dll
